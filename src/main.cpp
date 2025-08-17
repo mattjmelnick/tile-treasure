83442b1e0c56a4f4f25f4619df9981032cd99c14
mattjmelnick <49130549+mattjmelnick@users.noreply.github.com>
2025-08-17 00:56:47 -0400

    Create BoardSquare struct to store square data, display values on board

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,44 @@ const int BORDER_WIDTH = 1;
 const int FRAME_THICKNESS = 3;
 const int BOARD_OFFSET = (SCREEN_WIDTH - (BOARD_SIZE * SQUARE_SIZE) - 400) / 2;
 
-// position the board on the screen
+// board's starting position
 int startX = BOARD_OFFSET;
 int startY = BOARD_OFFSET;
 
-std::vector<int> randomizeWeights();
+struct BoardSquare
+{
+    int height = SQUARE_SIZE;
+    int width = SQUARE_SIZE;
+    int row;
+    int col;
+    int posX;
+    int posY;
+    int value;
+    int weight;
+    bool visited = false;
+};
+
+std::vector<int> createWeightsVector();
+std::vector<int> createValuesVector();
+std::vector<int> randomizeVector(std::vector<int> vector);
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH + 200, SCREEN_HEIGHT + 200, "Tile Treasure");
     SetTargetFPS(60);
 
-    std::vector<int> weightsVector = randomizeWeights();
-    std::vector<std::vector<int>> board(BOARD_SIZE, std::vector<int>(BOARD_SIZE));
+    std::vector<int> valuesVector = createValuesVector();
+    std::vector<int> weightsVector = createWeightsVector();
 
+    valuesVector = randomizeVector(valuesVector);
+    weightsVector = randomizeVector(weightsVector);
+
+    std::vector<std::vector<BoardSquare>> board(BOARD_SIZE, std::vector<BoardSquare>(BOARD_SIZE));
+
+    int valuesIndex = 0;
     int weightsIndex = 0;
 
-    // fill board with random weights in weightsVector
+    // fill board with random values and weights
     for (int row = 0; row < BOARD_SIZE; row++)
     {
         for (int col = 0; col < BOARD_SIZE; col++)
@@ -44,7 +65,12 @@ int main(void)
                 continue;
             }
 
-            board[row][col] = weightsVector[weightsIndex];
+            board[row][col].row = row;
+            board[row][col].col = col;
+            board[row][col].value = valuesVector[valuesIndex];
+            board[row][col].weight = weightsVector[weightsIndex];
+
+            valuesIndex++;
             weightsIndex++;
         }
     }
@@ -64,16 +90,26 @@ int main(void)
                 int posX = startX + (col * SQUARE_SIZE);
                 int posY = startY + (row * SQUARE_SIZE);
 
+                board[row][col].posX = posX;
+                board[row][col].posY = posY;
+
                 // draw the square and add a border
                 DrawRectangle(posX, posY, SQUARE_SIZE, SQUARE_SIZE, squareColor);
                 DrawRectangleLinesEx((Rectangle){(float)posX, (float)posY, (float)SQUARE_SIZE, (float)SQUARE_SIZE}, BORDER_WIDTH, BLACK);
 
+                // add value text to the squares
+                std::string valueText = std::to_string(board[row][col].value);
+                int valueTextWidth = MeasureText(valueText.c_str(), 25);
+                int valueTextX = posX + (SQUARE_SIZE / 2) - (valueTextWidth / 2);
+                int valueTextY = posY + (SQUARE_SIZE / 2) - 16;
+                DrawText(valueText.c_str(), valueTextX, valueTextY, 25, BLACK);
+
                 // add weight text to the squares
-                std::string weightText = std::to_string(board[row][col]);
-                int textWidth = MeasureText(weightText.c_str(), 15);
-                int textX = posX + (SQUARE_SIZE / 2) - (textWidth / 2);
-                int textY = posY + (SQUARE_SIZE / 2) + 8;
-                DrawText(weightText.c_str(), textX, textY, 15, BLACK);
+                std::string weightText = std::to_string(board[row][col].weight);
+                int weightTextWidth = MeasureText(weightText.c_str(), 15);
+                int weightTextX = posX + (SQUARE_SIZE / 2) - (weightTextWidth / 2);
+                int weightTextY = posY + (SQUARE_SIZE / 2) + 8;
+                DrawText(weightText.c_str(), weightTextX, weightTextY, 15, BLACK);
             }
         }
 
@@ -93,7 +129,7 @@ int main(void)
     return 0;
 }
 
-std::vector<int> randomizeWeights()
+std::vector<int> createWeightsVector()
 {
     std::vector<int> weights = {1, 2, 3, 4};
     std::vector<int> weightsVector;
@@ -107,21 +143,30 @@ std::vector<int> randomizeWeights()
         }
     }
 
-    // for (int n : weightsVector)
-    // {
-    //     std::cout << n << ' ';
-    // }
-    // std::cout << std::endl;
+    return weightsVector;
+}
 
+std::vector<int> createValuesVector()
+{
+    std::vector<int> values = {-4, -2, 2, 4, 6, 8};
+    std::vector<int> valuesVector;
+
+    for (int i : values)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            valuesVector.push_back(i);
+        }
+    }
+
+    return valuesVector;
+}
+
+std::vector<int> randomizeVector(std::vector<int> vector)
+{
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    std::shuffle(weightsVector.begin(), weightsVector.end(), generator);
+    std::shuffle(vector.begin(), vector.end(), generator);
 
-    // for (int n : weightsVector)
-    // {
-    //     std::cout << n << ' ';
-    // }
-    // std::cout << std::endl;
-
-    return weightsVector;
+    return vector;
 }
